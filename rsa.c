98bad8c18c50a470e12e724dc9389b50a3948847
bibimbup <74233697+bibimbup@users.noreply.github.com>
2021-11-09 23:43:07 +0800

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,16 +1,19 @@
 void asymEncrypt(double p, double q){
-	double n, z, d, gcd, e, c, encrypt, decrypt, i, m = 5;
+	double n, z, d, gcd, e, c, i;
+	int j, temp;
+	char m[50] = "LEAGUE";
+	int k[50];
 	
 	n = p*q;
 	z = (p-1)*(q-1);
 	printf("n=%.0f z=%.0f\n", n, z);
 	e = n-1;
 	printf("e = %.0f\n", e);
-	
+
 	do{
 		for(i=1; i<=e && i<= z; i++){
-    		if(fmod(e, i) == 0 && fmod(z, i) == 0){
-    			gcd = i;
+    			if(fmod(e, i) == 0 && fmod(z, i) == 0){
+    				gcd = i;
 			}
 		}
 		e = gcd + 1;
@@ -19,19 +22,37 @@ void asymEncrypt(double p, double q){
 	
 	printf("e = %.0f\n", e);
 	
-	for(d=0; fmod((e*d)-1, z) != 0; d++){}
+	for(d=0; ; d++){
+    		if(d!=z && (e*d)-1 != z){
+        		if(fmod((e*d)-1, z) ==0){
+            			break;
+     			}
+  		}
+	}  
 	
 	printf("d = %.0f\n", d);
 	
 	printf("\nPublic Key is (%.0f, %.0f)\n", n, e);
 	printf("Private Key is (%.0f, %.0f)\n", n, d);
 	
-	printf("\nm = %.0f\n", m);
-	c = fmod(pow(m,e), n);
-	printf("The encryption of m is c = %.0f\n", c);
+	printf("\nm = ");
+	for(j=0; m[j] != '\0'; j++){
+		printf("%c", m[j]);
+	}
 	
-	printf("\nc = %.0f\n", c);
-	m = fmod(pow(c,d), n);
-	printf("The decryption of c is m = %.0f\n", m);
+
+	printf("\n\nThe encryption of m is: ", c);
+	for(j = 0; (j < 50 && m[j] != '\0'); j++){
+		m[j] = tolower(m[j]);
+		temp = (int)(m[j])-96;
+		k[j] = fmod(pow(temp,e), n);
+		printf("%d ", k[j]);
+	}
+
+	printf("\nThe decryption of c is: ", m);
+	for(j = 0; (j < 50 && m[j] != '\0'); j++){
+		k[j] = ((int)fmod(pow(k[j]+96,d), n));
+		printf("%c ", k[j]);
+	}
 	
 }
